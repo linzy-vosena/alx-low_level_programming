b3fb0a35f67a176eee699271403d11fd6bbd3136
linzy-vosena <vihendalinzi@gmail.com>
2023-08-10 14:03:13 -0700

    intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+/**
+ * main -main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+	printf(""
+	printf("size of a char: %lu bite(s)\n" sizeof(char));
+        prinf("size of int: % lu byte(s)", sizeof(int));
+        printf("size of long int: % lu byte(s)", sizeof(long int));
+        printf("size of long long int: % lu byte(s)", sizeof(long long long int));
+        printf("size of a float: %lu byte(s)" sizeof(float));
+        return (0);
+}	
