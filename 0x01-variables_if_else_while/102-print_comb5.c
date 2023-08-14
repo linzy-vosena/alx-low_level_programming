1c7974502e1d7a1c8d1e9b6f5b8f82257e778930
linzy-vosena <vihendalinzi@gmail.com>
2023-08-14 02:24:57 -0700

    software

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,24 +3,24 @@
 /**
  * main - prints all possible combinations of two two-digit numbers
  *
- * Return: 0
+ * Return: 0 when successful
  */
 
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; j++)
+	for (i = 0 ; i <= 99 ; j++)
 	{
-		for (j = 0; j <= 99; j++)
+		for (j = 0 ; j <= 99 ; j++)
 		{
-			if (i < j && i != j)
+			if (i < j)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
 				if (i != 98 || j != 99)
 				{
 					putchar(',');
