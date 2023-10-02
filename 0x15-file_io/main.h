6ab7b1e94f99c217f761c5912d02236ef1ec1c3b
linzy-vosena <vihendalinzi@gmail.com>
2023-10-02 14:48:09 -0700

    Tread lightly, she is near

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,12 @@
-i#ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stddef.h>
+#include <stdio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
