cf314bc4ca546331cea928bcfd195d97778557b1
linzy-vosena <vihendalinzi@gmail.com>
2023-10-04 12:42:01 -0700

    copy

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,8 @@
 #include <fcntl.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
