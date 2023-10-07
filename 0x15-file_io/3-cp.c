f8fccb63984cfb432f8d6b440486fbf96a74be2c
linzy-vosena <vihendalinzi@gmail.com>
2023-10-06 19:28:50 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
 
+char *create_buffer(char *file);
+void close_file(int fd);
+
 /**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * create_buffer - Allocates 1024 bytes for a buffer
+ * @file: The name of the file buffer is storing chars for
  *
- * Return: A pointer to the newly-allocated buffer.
+ * Return: A pointer to the newly-allocated buffer
  */
 char *create_buffer(char *file)
 {
@@ -24,8 +27,8 @@ char *create_buffer(char *file)
 }
 
 /**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
+ * close_file - Closes file descriptors
+ * @fd: The file descriptor to be closed
  */
 void close_file(int fd)
 {
@@ -45,11 +48,11 @@ void close_file(int fd)
  * @argc: The number of arguments supplied to the program
  * @argv: An array of pointers to the arguments
  *
- * Return: 0 when sucessful
+ * Return: 0 when successful
  *
  * Description: If the argument count is incorrect - exit code 97
  *              If file_from does not exist or cannot be read - exit code 98
- *              If file_to can't be created or written to - exit code 99
+ *              If file_to cannot be created or written to - exit code 99
  *              If file_to or file_from cannot be closed - exit code 100
  */
 int main(int argc, char *argv[])
